68bc90b6cde9e7b1e8ed69f287de19fd33b74f64
peecode007 <codetech007@gmail.com>
2024-06-07 03:18:30 +0530

    Final

--- client/src/pages/Auth/AuthTabs.jsx
+++ client/src/pages/Auth/AuthTabs.jsx
@@ -13,8 +13,7 @@ const AuthTabs = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-cover" style={{ backgroundImage: 'url(/bg.jpg)' }}>
 
-      <div className="w-full max-w-md p-8  rounded shadow-lg bg-cover" style={{ backgroundImage: 'url(/bg.jpg)' }}>
-        <h1 className="text-2xl font-semibold mb-6 text-center bg-black">Authentication</h1>
+      <div className="w-full max-w-md p-8  rounded shadow-lg bg-orange-200" >
         <Tabs defaultValue={currentTab} value={currentTab} className="w-full">
           <TabsList className="grid w-full grid-cols-2">
             <TabsTrigger value="signup" onClick={() => navigate('/api/auth/signup')} className="text-center">Signup</TabsTrigger>
