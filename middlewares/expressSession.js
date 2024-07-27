a1ecc331a92b22f5e07d4f5ee1e4b62f71bd6805
peecode007 <codetech007@gmail.com>
2024-07-28 03:40:55 +0530

    Session

--- middlewares/expressSession.js
+++ middlewares/expressSession.js
@@ -7,7 +7,7 @@ const configuerExpressSession = (app) => {
         saveUninitialized: false,
         cookie: {
             httpOnly: true,
-            sameSite: 'Lax',
+            sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax',
             secure: process.env.NODE_ENV === 'production',
             maxAge: 60 * 60 * 1000, // 1 hour
         }
