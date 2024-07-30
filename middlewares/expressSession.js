aa0e4faeb5b56754034dc46e3965859b7e9bd2c6
peecode007 <codetech007@gmail.com>
2024-07-30 12:20:49 +0530

    Redis

--- middlewares/expressSession.js
+++ middlewares/expressSession.js
@@ -1,17 +1,24 @@
-import session from "express-session";
+import session from 'express-session';
+import RedisStore from 'connect-redis';
+import redis from 'redis';
 
-const configuerExpressSession = (app) => {
-    app.use(session({
-        secret: process.env.COOKIE_SECRET,
-        resave: false,
-        saveUninitialized: false,
-        cookie: {
-            httpOnly: true,
-            sameSite: 'None',
-            secure: process.env.NODE_ENV === 'production',
-            maxAge: 60 * 60 * 1000, // 1 hour
-        }
-    }));
+const redisClient = redis.createClient({
+  url: process.env.REDIS_URL, // Ensure this is set in your environment variables
+});
+
+const configureExpressSession = (app) => {
+  app.use(session({
+    store: new RedisStore({ client: redisClient }),
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+      sameSite: 'None', // Required for cross-origin requests
+      secure: process.env.NODE_ENV === 'production', // Ensure HTTPS
+      maxAge: 60 * 60 * 1000, // 1 hour
+    }
+  }));
 };
 
-export default configuerExpressSession;
+export default configureExpressSession;
