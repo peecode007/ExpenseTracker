98457e2ab708eca905aa5979c732656df9438b8b
peecode007 <codetech007@gmail.com>
2025-04-25 21:12:19 +0530

    Update

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,6 @@
 // src/config.js
 
-const API_URI = 'https://expense-tracker-mu-six-78.vercel.app/'; // Replace with your actual API URI
+// const API_URI = 'https://expense-tracker-mu-six-78.vercel.app/'; // Replace with your actual API URI
+const API_URI = "https://expensetracker-evyd.onrender.com"; 
 
 export default API_URI;
